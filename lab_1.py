3f76091f49821aa44fa5360710ba226f633e75fc
yaara21-meet <yaara21@meet.mit.edu>
2019-07-22 03:30:47 -0400

    Stopping here

---
+++ lab_1.py
@@ -0,0 +1,70 @@
+import turtle
+
+#the letter Y
+turtle.penup()
+turtle.goto(-400,0)
+turtle.pendown()
+turtle.goto(-400,150)
+turtle.goto(-500,300)
+turtle.penup()
+turtle.goto(-400,150)
+turtle.pendown()
+turtle.goto(-300,300)
+
+#the letter A
+turtle.penup()
+turtle.goto(-270,0)
+turtle.pendown()
+turtle.goto(-170,300)
+turtle.goto(-70,0)
+turtle.penup()
+turtle.goto(-220,150)
+turtle.pendown()
+turtle.goto(-120,150)
+
+
+
+
+
+#the letter A
+turtle.penup()
+turtle.goto(0,0)
+turtle.pendown()
+turtle.goto(100,300)
+turtle.goto(200,0)
+turtle.penup()
+turtle.goto(50,150)
+turtle.pendown()
+turtle.goto(150,150)
+
+#the letter R
+turtle.penup()
+turtle.goto(250,0)
+turtle.pendown()
+turtle.goto(250,300)
+turtle.goto(325,275)
+turtle.goto(370,215)
+turtle.goto(370,185)
+turtle.goto(325,125)
+turtle.goto(250,100)
+turtle.goto(370,0)
+
+#the letter A
+turtle.penup()
+turtle.goto(420,0)
+turtle.pendown()
+turtle.goto(520,300)
+turtle.goto(620,0)
+turtle.penup()
+turtle.goto(470,150)
+turtle.pendown()
+turtle.goto(570,150)
+
+
+
+
+
+
+
+
+turtle.mainloop()
