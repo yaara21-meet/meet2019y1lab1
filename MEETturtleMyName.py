55852365bb81a30b713f80952092c739f5ac3301
yaara21-meet <yaara21@meet.mit.edu>
2019-07-22 02:11:50 -0400

    Stopping here

---
+++ MEETturtleMyName.py
@@ -0,0 +1,48 @@
+import turtle
+
+#the letter Y
+turtle.penup()
+turtle.goto(-400,0)
+turtle.pendown()
+turtle.goto(-400,150)
+turtle.goto(-500,300)
+turtle.penup()
+turtle.goto(-400,150)
+turtle.pendown()
+turtle.goto(-300,300)
+
+#the letter A
+turtle.penup()
+turtle.goto(-250,0)
+turtle.pendown()
+turtle.goto(-150,300)
+turtle.goto(-50,0)
+turtle.penup()
+turtle.goto(-200,150)
+turtle.pendown()
+turtle.goto(-100,150)
+
+
+
+
+
+#the letter A
+turtle.penup()
+turtle.goto(0,0)
+turtle.pendown()
+turtle.goto(100,300)
+turtle.goto(200,0)
+turtle.penup()
+turtle.goto(50,150)
+turtle.pendown()
+turtle.goto(150,150)
+
+#the letter R
+
+
+
+
+
+
+
+turtle.mainloop()
